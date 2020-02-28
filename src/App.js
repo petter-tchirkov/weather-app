666f450a7bf4786fd667e85ba24dd67a7d1b0b46
Petter Tchirkov <54173272+petter-tchirkov@users.noreply.github.com>
2020-02-28 23:37:09 +0200

    First

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Form from './components/form'
 import Weather from './components/weather'
 
 const API_KEY = `7250b4aa5d0ccdb8e2179a7a8f4185ec`;
-class App extends React.Component{
+
+
+class App extends React.Component {
+
+
 
   state = {
     temp: undefined,
@@ -16,33 +20,52 @@ class App extends React.Component{
     error: undefined,
     lon: undefined,
     lat: undefined,
-    weather: undefined
+    weather: undefined,
+    data: undefined
+  }
+  dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+      "August", "September", "October", "November", "December"];
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
   }
 
   getWeather = async (e) => {
 
     e.preventDefault();
     const city = e.target.elements.city.value;
-    
 
-    if(city){
-      const api_url = await 
-      fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
+
+    if (city) {
+      const api_url = await
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
       const data = await api_url.json();
       console.log(data);
 
+
       this.setState({
-        // temp: data.main.temp,
-        // city: data.name,
-        // country: data.sys.country,
-        // maxTemp: data.main.temp_max,
-        // minTemp: data.main.temp_min,
-        // lat: data.coord.lat,
-        // lon: data.coord.lon,
-        // weather: data.weather[0].main,
-        // error: "Please enter the city"
+        temp: data.list[0],
+        city: data.city.name,
+        country: data.city.country,
+        error: undefined,
+        data: data,
+        lat: data.city.coord.lat,
+        lon: data.city.coord.lon
       });
-    } else{
+    } else {
       this.setState({
         temp: undefined,
         city: undefined,
@@ -57,21 +80,23 @@ class App extends React.Component{
     }
   }
 
-  render(){
+  render() {
     return (
       <div>
         <Info />
         <Form weatherMethod={this.getWeather} />
-        <Weather 
-        temp={this.state.temp}
-        city={this.state.city}
-        country={this.state.country}
-        maxTemp={this.state.maxTemp}
-        minTemp={this.state.minTemp}
-        lat={this.state.lat}
-        lon={this.state.lon}
-        weather={this.state.weather}
-        error={this.state.error}
+        <Weather
+          data={this.state.data}
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          maxTemp={this.state.maxTemp}
+          minTemp={this.state.minTemp}
+          lat={this.state.lat}
+          lon={this.state.lon}
+          weather={this.state.weather}
+          error={this.state.error}
+          date={this.dateBuilder}
         />
       </div>
     )
--- src/components/info.js
+++ src/components/info.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import App from '../App'
-
-class Info extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>Know the Weather</h1>
-                <p>in your city</p>
-            </div>
+import '../css/info.css'
+class Info extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>Weather Application</h1>
+                <p>5 day Forecast for every 3 hours</p>
+            </header>
         )
     }
 }
